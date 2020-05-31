bab49e40460ef9b3bac99ea2d31e68eca1c4d14c
Saraaaaaaaaaaaaaaa <saradjurovic97@gmail.com>
2020-05-31 13:02:02 +0200

    Fixing button styling on SingIn component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {auth} from './firebase/firebase.utils'
 import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sing-up/sign-in-and-sing-up.component'
 
 import './App.css';
 
@@ -11,20 +14,42 @@ const TestPage = (props) => (
   </div>
 )
 
-function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path='/' component={HomePage}></Route>
-        <Route exact path='/shop' component={ShopPage}></Route>
-        <Route exact path='/shop/hats' component={TestPage}></Route>
-        <Route exact path='/shop/jackets' component={TestPage}></Route>
-        <Route exact path='/shop/sneakers' component={TestPage}></Route>
-        <Route exact path='/shop/womens' component={TestPage}></Route>
-        <Route exact path='/shop/mens' component={TestPage}></Route>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {this.setState({currentUser: user})});
+  }
+
+  render(){
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}></Header>
+        <Switch>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route exact path='/shop' component={ShopPage}></Route>
+          <Route exact path='/signin' component={SignInAndSignUpPage}></Route>
+          <Route exact path='/shop/hats' component={TestPage}></Route>
+          <Route exact path='/shop/jackets' component={TestPage}></Route>
+          <Route exact path='/shop/sneakers' component={TestPage}></Route>
+          <Route exact path='/shop/womens' component={TestPage}></Route>
+          <Route exact path='/shop/mens' component={TestPage}></Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
