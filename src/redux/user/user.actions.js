224497c182529551c80e864be1adc2f21e2c6367
Saraaaaaaaaaaaaaaa <saradjurovic97@gmail.com>
2020-06-08 16:26:54 +0200

    Fixed GIT errors

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import {UserActionTypes} from './user.types';
 
 export const setCurrentUser = user => ({
     type: UserActionTypes.SET_CURRENT_USER,
-=======
-export const setCurrentUser = user => ({
-    type: 'SET_CURRENT_USER',
->>>>>>> refs/remotes/origin/master
     payload: user
 })
--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
 import {UserActionTypes} from './user.types';
 
-=======
->>>>>>> refs/remotes/origin/master
 const INITIAL_STATE = {
     currentUser: null
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-<<<<<<< HEAD
         case UserActionTypes.SET_CURRENT_USER:
-=======
-        case 'SET_CURRENT_USER':
->>>>>>> refs/remotes/origin/master
             return {
                 ...state, currentUser: action.payload
             }
